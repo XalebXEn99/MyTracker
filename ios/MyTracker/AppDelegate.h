19e7ccaf407b7f448653bb2111a8ddc716de2567
XalebXEn99 <150833019+XalebXEn99@users.noreply.github.com>
2026-06-03 19:19:41 +0200

    cache: remove ios from index for renormalization

--- ios/MyTracker/AppDelegate.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-#import <React/RCTBridgeDelegate.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
-@property (nonatomic, strong) UIWindow *window;
-@end
